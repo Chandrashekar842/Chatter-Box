8140a2a412c2acbaaab80031a9c6abb578727ee3
Chandra Shekar <mchandrashekar842@gmail.com>
2024-08-17 13:19:19 +0530

    new state for notification added

--- frontend/src/context/ChatProvider.jsx
+++ frontend/src/context/ChatProvider.jsx
@@ -7,10 +7,11 @@ export const ChatProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [selectedChat, setSelectedChat] = useState();
   const [chats, setChats] = useState([]);
+  const [notification, setNotification] = useState([])
 
   return (
     <ChatContext.Provider
-      value={{ selectedChat, setSelectedChat, chats, setChats }}
+      value={{ selectedChat, setSelectedChat, chats, setChats, notification, setNotification }}
     >
       {children}
     </ChatContext.Provider>
